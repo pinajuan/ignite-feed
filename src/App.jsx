1b79e6d3922b4d13546e516b70c1c551bdc1c751
Juan Pina <juanpinaribeiro@outlook.com>
2025-04-25 15:16:21 -0300

    Post criado

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar';
+import { Post } from './components/Post';
 
 import styles from './App.module.css';
 import './global.css'
@@ -11,7 +12,11 @@ function App() {
       <Header/>
       <div className={styles.wrapper}>
         <Sidebar/>
-        <main></main>
+        <main>
+          <Post/>
+          <Post/>
+          <Post/>
+        </main>
       </div>
     </div>
   )

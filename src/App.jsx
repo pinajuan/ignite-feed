28ed51e01e9fdfc3f347df07855426c6c12166af
Juan Pina <juanpinaribeiro@outlook.com>
2025-04-25 14:30:23 -0300

    Sidebar criado

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar';
 
+import styles from './App.module.css';
 import './global.css'
 
 function App() {
@@ -7,7 +9,10 @@ function App() {
   return (
     <div>
       <Header/>
-      Hello World
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main></main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/pinajuan.png" />
+                <strong>Diego Fernandes</strong>
+                <span>Web Developer</span>
+            </div>
+            <footer>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}

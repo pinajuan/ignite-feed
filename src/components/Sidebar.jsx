a4bec0100b140439bec6c45e3831572be5bb2e4a
Juan Pina <juanpinaribeiro@outlook.com>
2025-04-25 17:18:23 -0300

    Avatar criado

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import { PencilLine } from 'phosphor-react';
 
+import { Avatar } from './Avatar';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -7,8 +9,8 @@ export function Sidebar() {
         <aside className={styles.sidebar}>
             <img className={styles.cover} src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/pinajuan.png" />
-                <strong>Diego Fernandes</strong>
+                <Avatar  src="https://github.com/pinajuan.png"/>
+                <strong>Juan Pina</strong>
                 <span>Web Developer</span>
             </div>
             <footer>

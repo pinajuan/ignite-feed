fd3f690768314ba05f1a8621876f3a5ca3b8f29b
Juan Pina <juanpinaribeiro@outlook.com>
2025-05-02 12:43:47 -0300

    Validando o botão "Publicar" do comentário

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -31,7 +31,12 @@ export function Post({ author, publishedAt, content }) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity("")
         setNewCommentText(event.target.value)
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity("Este campo é obrigatório!")
     }
 
     function deleteComment(commentToDelete) {
@@ -40,6 +45,8 @@ export function Post({ author, publishedAt, content }) {
         })
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length == 0;
 
     return (
         <article className={styles.post}>
@@ -71,9 +78,17 @@ export function Post({ author, publishedAt, content }) {
 
             <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
-                <textarea name="comment" value={newCommentText} placeholder="Deixe um comentário" onChange={handleNewCommentChange} />
+                <textarea
+                    name="comment"
+                    value={newCommentText}
+                    placeholder="Deixe um comentário"
+                    onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required/>
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
 

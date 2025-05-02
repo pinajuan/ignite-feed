7eef876957e883353cf4793ed8b601ce14e195a2
Juan Pina <juanpinaribeiro@outlook.com>
2025-05-02 12:21:41 -0300

    Removendo os comentários (ícone de deletar)

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,7 +4,12 @@ import { Avatar } from './Avatar';
 
 import styles from './Comment.module.css';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+ 
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/pinajuan.png" alt="" />
@@ -15,7 +20,7 @@ export function Comment({ content }) {
                             <strong>Juan Pina</strong>
                             <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">Cerca de 1h atrás</time>
                         </div>
-                        <button title="Deletar comentário">
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
                             <Trash size={24} />
                         </button>
                     </header>
